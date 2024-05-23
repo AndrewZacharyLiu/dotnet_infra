6ca3dc94f76c09da71a1d67c39914d2ace3115a7
AndrewZacharyLiu <87785681+AndrewZacharyLiu@users.noreply.github.com>
2024-05-22 21:45:13 -0700

    NET 6.0 Mongo

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,13 @@
 # Use the official ASP.NET Core runtime as a parent image
-FROM mcr.microsoft.com/dotnet/sdk:3.1.426 AS base
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS base
 WORKDIR /app
 COPY . .
-RUN apt-get update && apt-get install -y curl && \
-    curl -fsSL https://deb.nodesource.com/setup_16.x | bash - && \
-    apt-get install -y nodejs
-RUN npm install eslint babel-eslint eslint-plugin-import eslint-config-react-app
-RUN dotnet restore "./reacttemp.csproj" --disable-parallel
-RUN dotnet publish "reacttemp.csproj" -c release -o /app/publish
 
-FROM mcr.microsoft.com/dotnet/sdk:3.1.426
+RUN dotnet restore "./ECommerce.Api.csproj" --disable-parallel
+RUN dotnet publish "ECommerce.Api.csproj" -c release -o /app/publish
+
+FROM mcr.microsoft.com/dotnet/sdk:6.0
 WORKDIR /app
 COPY --from=base /app/publish ./
-EXPOSE 8002
-ENTRYPOINT ["dotnet", "reacttemp.dll"]
+ENV DOTNET_URLS=http://+:8002
+ENTRYPOINT ["dotnet", "ECommerce.Api.dll"]
--- Program.cs
+++ Program.cs
@@ -1,27 +1,58 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using ECommerce.Api.Helpers;
+using ECommerce.Api.Services.Contract;
+using ECommerce.Api.Services;
+using ECommerce.Api.Domain;
+using ECommerce.Api.Infra.Repositories.CacheRepositories.MongoCacheRepositories;
+using ECommerce.Api.Infra.Repositories.ReadRepositories.ProductReadRepositories;
+using ECommerce.Api.Infra.Repositories.WriteRepositories.ProductWriteRepositories;
 
-namespace reacttemp
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>()
-                              .UseUrls("http://0.0.0.0:8002");
-                });
-    }
-}
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add services to the container.
+builder.Services.Configure<ProductDatabaseSettings>(
+    builder.Configuration.GetSection("ProductDatabaseSettings"));
+
+#region [ IOC ]
+
+#region [ Application ]
+
+builder.Services.AddScoped<IProductService, ProductService>();
+
+#endregion [Application]
+
+#region [ Cache ]
+
+builder.Services.AddScoped<IMongoCacheRepository, MongoCacheRepository>();
+
+#endregion [ Cache ]
+
+#region [ Infra - Data ]
+
+builder.Services.AddScoped<IProductReadRepository, ProductReadRepository>();
+builder.Services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+
+#endregion [ Infra - Data EventSourcing ]
+
+#endregion [ IOC ]
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+//in production, this should be commented out
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
